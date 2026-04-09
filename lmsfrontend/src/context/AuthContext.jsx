638f42e2f7335e1d8ca37f874f9da9cfa1035f08
Narendra kanakam <kanakamnarendra173@gmail.com>
2026-04-09 14:56:37 +0530

    url ud

--- lmsfrontend/src/context/AuthContext.jsx
+++ lmsfrontend/src/context/AuthContext.jsx
@@ -2,7 +2,7 @@ import React, { createContext, useState, useEffect } from 'react';
 import axios from 'axios';
 
 export const AuthContext = createContext();
-export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000';
+export const BACKEND_URL = https://narilmsbackend.onrender.com || 'http://localhost:5000';
 
 export const AuthProvider = ({ children }) => {
     const url = BACKEND_URL;
